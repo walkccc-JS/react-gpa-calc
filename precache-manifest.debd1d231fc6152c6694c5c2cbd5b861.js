4dba2ff65f622493738366094db1ddd48cd9c479
Jay Chen <walkccray@gmail.com>
2019-10-03 22:16:24 +0800

    Updates

---
+++ precache-manifest.debd1d231fc6152c6694c5c2cbd5b861.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517edcaca9f21896d4b0f21eb2dbca5a",
+    "url": "/react-gpa-calc/index.html"
+  },
+  {
+    "revision": "7046b225b011a90f8362",
+    "url": "/react-gpa-calc/static/css/2.9f4a86d1.chunk.css"
+  },
+  {
+    "revision": "eeae99687b4f9cc03687",
+    "url": "/react-gpa-calc/static/css/main.90699b92.chunk.css"
+  },
+  {
+    "revision": "7046b225b011a90f8362",
+    "url": "/react-gpa-calc/static/js/2.035282e3.chunk.js"
+  },
+  {
+    "revision": "eeae99687b4f9cc03687",
+    "url": "/react-gpa-calc/static/js/main.23c6f089.chunk.js"
+  },
+  {
+    "revision": "bcaf484896a3907150fc",
+    "url": "/react-gpa-calc/static/js/runtime-main.ce9e585e.js"
+  },
+  {
+    "revision": "2cee66fff8f2d459a6174faf557c087a",
+    "url": "/react-gpa-calc/static/media/aplus.2cee66ff.png"
+  },
+  {
+    "revision": "9c46102545d7f5aa4419ba28f69bee5b",
+    "url": "/react-gpa-calc/static/media/github.9c461025.png"
+  },
+  {
+    "revision": "8b201685859c6b291fb39019e19896fd",
+    "url": "/react-gpa-calc/static/media/google.8b201685.png"
+  }
+]);
